fa8961e9d73c240d6d70125b141de668f388edf5
enrique-bustamante <enrique.bustamante22@gmail.com>
2020-08-19 20:47:12 -0500

    Completed all of the challenge with the exception of the gauge.

--- plots.js
+++ plots.js
@@ -1,37 +1,121 @@
-// Sort the data array using the greekSearchResults value
-data.sort(function(a, b) {
-  return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
-});
+function init() {
+  var selector = d3.select("#selDataset");
 
-// Slice the first 10 objects for plotting
-data = data.slice(0, 10);
+  d3.json("samples.json").then((data) => {
+    console.log(data);
+    var sampleNames = data.names;
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+})}
 
-// Reverse the array due to Plotly's defaults
-data = data.reverse();
+init();
 
-// Trace1 for the Greek Data
-var trace1 = {
-  x: data.map(row => row.greekSearchResults),
-  y: data.map(row => row.greekName),
-  text: data.map(row => row.greekName),
-  name: "Greek",
-  type: "bar",
-  orientation: "h"
-};
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    var PANEL = d3.select("#sample-metadata");
 
-// data
-var data = [trace1];
+    PANEL.html("");
+    PANEL.append("h6").text("ID: "+result.id);
+    PANEL.append("h6").text("ETHNICITY: "+result.ethnicity);
+    PANEL.append("h6").text("GENDER: "+result.gender);
+    PANEL.append("h6").text("AGE: "+result.age);
+    PANEL.append("h6").text("LOCATION: "+result.location);
+    PANEL.append("h6").text("BBTYPE: "+result.bbtype);
+    PANEL.append("h6").text("WFREQ: "+result.wfreq);
+  });
+}
 
-// Apply the group bar mode to the layout
-var layout = {
-  title: "Greek gods search results",
-  margin: {
-    l: 100,
-    r: 100,
-    t: 100,
-    b: 100
+function buildCharts(sample){
+  d3.json("samples.json").then((data) => {
+    var samples = data.samples;
+    var sampleArray = samples.filter(sampleObj => sampleObj.id == sample);
+    var sampleList = sampleArray[0];
+    var topOtuIds = sampleList.otu_ids.slice(0,10).map(item => 'OTU '+item);
+    var topSampleValues = sampleList.sample_values.slice(0,10);
+    var topOtuLabels = sampleList.otu_labels.slice(0,10);
+    var otuIds = sampleList.otu_ids;
+    var sampleValues = sampleList.sample_values;
+    var otuLabels = sampleList.otu_labels;
+
+  // Build bar chart
+  var trace1 = [{
+    x: topSampleValues.reverse(),
+    y: topOtuIds.reverse(),
+    type: "bar",
+    text: topOtuLabels,
+    orientation: 'h'
+  }];
+  console.log(otuIds)
+  Plotly.newPlot("bar", trace1);
+
+  // Build bubble chart
+  var trace3 = [{
+    x: otuIds,
+    y: sampleValues,
+    mode: 'markers',
+    marker:{
+      size: sampleValues,
+      color: otuIds
+    },
+    labels: otuLabels
+  }];
+  layout = {
+    xaxis: {title: "OTU ID"},
+    };
+  Plotly.newPlot("bubble", trace3, layout)
+  });
+
+  // Build gauge chart
+d3.json("samples.json").then((data) => {
+  var metadata = data.metadata;
+  var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+  var result = resultArray[0];
+  var trace2 = [
+    {
+    domain: { x: [0, 1], y: [0, 1] },
+    value: result.wfreq,
+    title: { text: "Belly Button Washing Frequency" },
+    type: "indicator",
+    mode: "gauge+number",
+    gauge: {
+      axis: { range: [0, 9] },
+      steps: [
+        { range: [0, 1], color: "white" },
+        { range: [1, 2], color: "LightGray" },
+        { range: [2, 3], color: "GreenYellow" },
+        { range: [3, 4], color: "LightGreen" },
+        { range: [4, 5], color: "YellowGreen" },
+        { range: [5, 6], color: "MediumSeaGreen" },
+        { range: [6, 7], color: "ForestGreen" },
+        { range: [7, 8], color: "Green" },
+        { range: [8, 9], color: "DarkGreen" },
+      ]
+    }
+    
+  
   }
-};
+  ];
+  
+ 
+var layout = {
+    width: 600, 
+    height: 500, 
+    margin: { t: 0, b: 0 } 
+  };
+  Plotly.newPlot('gauge', trace2, layout);
+});
+  
+}
 
-// Render the plot to the div tag with id "plot"
-Plotly.newPlot("plot", data, layout);
+function optionChanged(newSample) {
+  buildMetadata(newSample);
+  buildCharts(newSample);
+}
+optionChanged(940);
